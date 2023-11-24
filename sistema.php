4e2e5363b5f9068f7b476b827dd1f232bbfc1eb4
regnasc <regismdn79@gmail.com>
2023-11-24 16:20:36 -0300

    mudando raiz

--- sistema.php
+++ sistema.php
@@ -28,7 +28,7 @@ $result = $conexao->query($sql);
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="home.html">SK2R-Security CRUD</a>
+            <a class="navbar-brand" href="index.html">SK2R-Security CRUD</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
